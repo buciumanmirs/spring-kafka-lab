f7612106390b7ecbd71bfd1f0313db4c10e5c24f
mirceabuciuman <mircea.buciuman93@gmail.com>
2025-09-12 18:37:43 +0300

    feat(Section 13): Order App - Add Header to Kafka Message

---
+++ docs/section13/Add_Header_To_Kafka_Messages.md
@@ -0,0 +1,5 @@
+# Add Header To Kafka Messages
+
+![img_23.png](img_23.png)
+
+![img_24.png](img_24.png)
--- kafka-ms-order/src/main/java/dev/mirotech/kafka/broker/producer/OrderProducer.java
+++ kafka-ms-order/src/main/java/dev/mirotech/kafka/broker/producer/OrderProducer.java
@@ -3,8 +3,14 @@ package dev.mirotech.kafka.broker.producer;
 import dev.mirotech.kafka.broker.message.OrderMessage;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.header.Header;
+import org.apache.kafka.common.header.internals.RecordHeader;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
+
+import java.util.ArrayList;
 
 @Slf4j
 @Service
@@ -14,7 +20,8 @@ public class OrderProducer {
     private final KafkaTemplate<String, OrderMessage> kafkaTemplate;
 
     public void sendOrder(OrderMessage orderMessage) {
-        kafkaTemplate.send("t-commodity-order", orderMessage.getOrderNumber(), orderMessage)
+        var producerRecord = buildProducerRecord(orderMessage);
+        kafkaTemplate.send(producerRecord)
                 .whenComplete((recordMetadata, exception) -> {
                     if (exception == null) {
                         log.info("Order {} sent successfully!", orderMessage.getOrderNumber());
@@ -24,6 +31,16 @@ public class OrderProducer {
                 });
 
         log.info("Just a dummy message for order: {} and item :{}", orderMessage, orderMessage.getItemName());
+    }
+
+    private ProducerRecord<String, OrderMessage> buildProducerRecord(OrderMessage orderMessage) {
+        var surpriseBonus = StringUtils.startsWithIgnoreCase(orderMessage.getOrderLocation(), "A") ? 25 : 15;
+        var kafkaHeaders = new ArrayList<Header>();
+        var surpriseBonusHeader = new RecordHeader("surpriseBonus", Integer.toString(surpriseBonus).getBytes());
+        kafkaHeaders.add(surpriseBonusHeader);
+
+        return new ProducerRecord<>("t-commodity-order", null, orderMessage.getOrderNumber(), orderMessage, kafkaHeaders);
+
     }
 }
 
