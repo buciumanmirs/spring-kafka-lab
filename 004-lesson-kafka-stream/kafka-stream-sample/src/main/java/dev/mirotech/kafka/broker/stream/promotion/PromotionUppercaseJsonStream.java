43deb571e23cc36863b31f2990a569a2b396cafa
mirceabuciuman <mircea.buciuman93@gmail.com>
2025-09-13 14:25:59 +0300

    feat (section 15 - Kafka Stream): using string serde, config PromotionUppercaseJsonStream

---
+++ 004-lesson-kafka-stream/kafka-stream-sample/src/main/java/dev/mirotech/kafka/broker/stream/promotion/PromotionUppercaseJsonStream.java
@@ -0,0 +1,44 @@
+package dev.mirotech.kafka.broker.stream.promotion;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import dev.mirotech.kafka.broker.message.message.PromotionMessage;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.kafka.common.serialization.Serdes;
+import org.apache.kafka.streams.StreamsBuilder;
+import org.apache.kafka.streams.kstream.Consumed;
+import org.apache.kafka.streams.kstream.Printed;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+@Slf4j
+@RequiredArgsConstructor
+public class PromotionUppercaseJsonStream {
+
+    private final ObjectMapper objectMapper;
+
+    @Autowired
+    void kstreamPromotionUppercase(StreamsBuilder builder) {
+        var sourceStream = builder.stream("t-commodity-promotion", Consumed.with(Serdes.String(), Serdes.String()));
+        var uppercaseStream = sourceStream.mapValues(this::uppercasePromotionCode);
+        uppercaseStream.to("t-commodity-promotion-uppercase");
+
+        // useful for debugging, but it is better not to use this on production
+
+        sourceStream.print(Printed.<String, String>toSysOut().withLabel("JSON Original Stream"));
+        uppercaseStream.print(Printed.<String, String>toSysOut().withLabel("JSON Uppercase Stream"));
+
+    }
+
+    private String uppercasePromotionCode(String jsonString) {
+        try {
+            var promotion = objectMapper.readValue(jsonString, PromotionMessage.class);
+            promotion.setPromotionCode(promotion.getPromotionCode().toUpperCase());
+            return objectMapper.writeValueAsString(promotion);
+        } catch (Exception e) {
+            log.error("Error parsing promotion message: {}", jsonString, e);
+            return "";
+        }
+    }
+}
